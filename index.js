d65c8eed2ed8973117d3edf96fe6abd4c7330364
codeconcept <contact@codeconcept.fr>
2018-06-13 17:21:30 +0200

    utilisation des promises

--- index.js
+++ index.js
@@ -1,49 +1,18 @@
-// **** utilisation de fonction qui retourne un résultat == sans callback
-// const synchronous = require('./asynchronous/synchronous');
-
-// const result = synchronous.getRandomNumberSync(10);
-// console.log(`nombre généré : ${result}`);
-
-// let i = 0;
-// const handle = setInterval(() => {
-//   let result = synchronous.getRandomNumberSync(10);
-//   console.log(`nombre généré dans setInterval : ${result}`);
-//   i++;
-//   if (i === 10) {
-//     clearInterval(handle);
-//     console.log('terminé');
-//   }
-// }, 50);
-
-// const total = synchronous.addSync(2, 5);
-// console.log(`total : ${total}`);
-// **** utilisation de fonction qui retourne un résultat == sans callback (end)
-
-// **** callback
-const asynchronous = require('./asynchronous/asynchronous_1');
-
+// **** promises
 console.log('avant getRandomNumber()');
+const { getRandomNumber } = require('./asynchronous/promises');
 
-asynchronous.getRandomNumber(10, (err, res) => {
-  if (err) throw err;
-  console.log(`nombre généré par getRandomNumber() : ${res}`);
-});
-
+getRandomNumber(5)
+  .then(data => console.log(`résultat : ${data}`))
+  .catch(error => console.log(`erreur : ${error.message}`));
 console.log('après getRandomNumber()');
+// **** promises (end)
 
-// console.log('avant getRandomNumber() qui va provoquer une erreur');
+// // **** promises reject
+// const { getRandomNumber } = require('./asynchronous/promises');
 
-// // string passée au lieu d'un number : provoquera une erreur
-// asynchronous.getRandomNumber('azerty', (err, res) => {
-//   if (err) throw err;
-//   console.log(`nombre généré par getRandomNumber() : ${res}`);
-// });
+// getRandomNumber('salut')
+//   .then(data => console.log(`résultat : ${data}`))
+//   .catch(error => console.log(`erreur : ${error.message}`));
 
-// console.log('après getRandomNumber() qui va provoquer une erreur');
-
-asynchronous.add(2, 5, (error, result) => {
-  if (error) console.error(`erreur : `, error.message);
-  console.log(`résultat de add(2, 5) : ${result}`);
-});
-
-// **** callback (end)
+// // **** promises reject (end)
