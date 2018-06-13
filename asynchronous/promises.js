6401b5781ac77c34cced04ed2adefb67fb062679
codeconcept <contact@codeconcept.fr>
2018-06-13 18:17:03 +0200

    permettre d'appeler une fonction en utilisant un callback ou une promise

--- asynchronous/promises.js
+++ asynchronous/promises.js
@@ -1,9 +1,22 @@
-exports.getRandomNumber = (maxValue) => {
+exports.getRandomNumber = (maxValue, cb) => {
+  const errorMessage = 'veuillez passer un nombre';
+  if (cb !== undefined) {
+    setTimeout(() => {
+      const result = Math.floor(Math.random() * maxValue);
+      if (typeof maxValue !== 'number') {
+        const error = new Error(errorMessage);
+        return cb(error);
+      }
+      return cb(null, result);
+    }, 0);
+  }
+
   return new Promise((resolve, reject) => {
     const result = Math.floor(Math.random() * maxValue);
     if (typeof maxValue !== 'number') {
-      return reject(new Error('veuillez passer un nombre'));
+      const error = new Error(errorMessage);
+      reject(error);
     }
-    resolve(result);
+    return resolve(result);
   });
 };
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-// **** promises
-console.log('avant getRandomNumber()');
 const { getRandomNumber } = require('./asynchronous/promises');
 
+console.log('avant getRandomNumber()');
+// **** passage de deux arguments : un nombre et un callback
+getRandomNumber(5, (err, result) => {
+  if (err) {
+    console.log(`erreur : ${err.message}`);
+    throw err;
+  }
+  console.log(`résultat : ${result}`);
+});
+console.log('après getRandomNumber()');
+// **** passage de deux arguments : un nombre et un callback (end)
+
+console.log('avant getRandomNumber()');
+// **** passage d'un seul argument : le nombre seul
 getRandomNumber(5)
   .then(data => console.log(`résultat : ${data}`))
   .catch(error => console.log(`erreur : ${error.message}`));
 console.log('après getRandomNumber()');
-// **** promises (end)
-
-// // **** promises reject
-// const { getRandomNumber } = require('./asynchronous/promises');
-
-// getRandomNumber('salut')
-//   .then(data => console.log(`résultat : ${data}`))
-//   .catch(error => console.log(`erreur : ${error.message}`));
-
-// // **** promises reject (end)
+// **** passage d'un seul argument : le nombre seul (end)
